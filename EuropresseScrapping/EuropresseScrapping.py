f0413b738e0a49ad64cb65ff89f423808d3b297a
jcoin <jcoin>
2024-08-24 12:37:08 +0200

    2024 version - Telegram and last edition retrieval

--- EuropresseScrapping/EuropresseScrapping.py
+++ EuropresseScrapping/EuropresseScrapping.py
@@ -1,70 +1,184 @@
+#!/srv/selenium/bin/python3
 
 import os,sys,getopt,configparser,glob
-from PyPDF2 import PdfFileReader, PdfFileWriter
+import asyncio
+
+from telegram import InlineQueryResultArticle, InputTextMessageContent , Bot
+from telegram.ext import Application, CommandHandler, InlineQueryHandler
+
+from time import sleep
+
+from PyPDF2 import PdfReader, PdfWriter
+
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver import   FirefoxProfile
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By 
-from time import sleep
 from selenium.common.exceptions import TimeoutException
-from datetime import date, timedelta
+from selenium.webdriver import ActionChains
+
+async def doc_tel(api_key,channel_id,pdf_file):
+
+   bot = Bot(api_key)
+   with open(pdf_file, "rb") as f:
+     await  bot.send_document(channel_id, f)
+
+async def msg_tel(api_key,channel_id,message):
+   bot = Bot(api_key)
+   await  bot.send_message(channel_id, message)
+
+
+def find_last_pdf(dir, id) :
+   print("Find Last PDF -- Start")  
+   glob_pattern_rename="/pdf_????????_"+id+".pdf"
+
+   pdfs_downloaded = sorted(filter( os.path.isfile,
+                            glob.glob(dir + glob_pattern_rename) ))
+
+   print(pdfs_downloaded)
+   if pdfs_downloaded :
+      return((pdfs_downloaded[-1])[30:38])
+   else: 
+      return ('')
+      
+   print("Find Last PDF -- End") 
+
+def rename_pdfs(paths):
+    for file in paths:
+      new_file=file[:-5]+"0"+file[-5:]
+      print("Rename to:"+new_file)
+      os.rename(file,new_file)
+
 
 def merge_pdfs(paths, output):
-    pdf_writer = PdfFileWriter()
+    pdf_writer = PdfWriter()
 
     for path in paths:
-        pdf_reader = PdfFileReader(path)
-        for page in range(pdf_reader.getNumPages()):
+        pdf_reader = PdfReader(path)
+        for page in range(len(pdf_reader.pages)):
             # Add each page to the writer object
-            pdf_writer.addPage(pdf_reader.getPage(page))
+            pdf_writer.add_page(pdf_reader.pages[page])
 
     # Write out the merged PDF
     with open(output, 'wb') as out:
         pdf_writer.write(out)
 
-def ScrappingTest(journalID,tomorrow_date,profile_path,gecko_path,auth_url,user,pwd):
+def testFinished(files):
+  result = False 
+  for file in files:
+    if (result == False):
+       result = file.endswith(".crdownload")
+  return (result)
     
-    ser = Service(gecko_path)
-    opt = FirefoxOptions()
-    opt.add_argument("-profile")
-    opt.add_argument(profile_path)
-    opt.headless=True
-    opt.set_preference("general.warnOnAboutConfig", False)
+def BNF_auth(auth,user,pwd):
+   opt = ChromeOptions()
+   opt.enable_downloads = True
+   opt.add_argument("--headless=new")
+   opt.add_experimental_option('prefs', {
+      "download.prompt_for_download": False, #To auto download the file
+      "download.directory_upgrade": True,
+      "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
+    })
+   print("BNF_auth -- Opt OK")
 
-    ff_driver = webdriver.Firefox(service=ser,options=opt)
 
-    ff_driver.get(auth_url)
-    ff_driver.maximize_window()
-    sleep(15)
-    username=ff_driver.find_element(by=By.NAME, value="username")
-    username.send_keys(user)
-    password=ff_driver.find_element(by=By.NAME, value="password")
-    password.send_keys(pwd)
-    password.submit()
-    sleep(3)
-    ff_driver.set_page_load_timeout(10)
-    ff_driver.get("https://nouveau-europresse-com.bnf.idm.oclc.org/WebPages/Pdf/SearchResult.aspx?DocName=pdf%C2%B7"+tomorrow_date+"%C2%B7"+journalID+"_P%C2%B71")
-    ff_driver.switch_to.frame('ListDoc')
-    list_of_links = ff_driver.find_elements(by=By.XPATH,value='//a')
-    to_scrape=[]
-    for link in list_of_links:
+   remote_driver = webdriver.Remote(
+      command_executor='http://localhost:4444/wd/hub',
+      options=opt)
+      
+      
+   print("BNF_auth -- Driver OK")
+
+   remote_driver.get(auth)
+
+   print("Wait+auth")
+   sleep(15)
+   username=remote_driver.find_element(by=By.NAME, value="username")
+   username.send_keys(user)
+   password=remote_driver.find_element(by=By.NAME, value="password")
+   password.send_keys(pwd)
+   password.submit()
+   sleep(10)
+   remote_driver.set_page_load_timeout(10)
+
+   return (remote_driver)
+  
+def ScrappingPdf(eDate,journalID,ff_driver):
+   print("ScrappingPdf -- Start")
+  
+   print(ff_driver.current_url) 
+   url_pub='https://nouveau-europresse-com.bnf.idm.oclc.org/Pdf'
+
+   ff_driver.get(url_pub)
+   sleep(2)
+
+   summaryPage="https://nouveau-europresse-com.bnf.idm.oclc.org/WebPages/Pdf/SearchResult.aspx?DocName=pdf%C2%B7"+eDate+"%C2%B7"+journalID+"_P%C2%B71"
+
+   print(summaryPage)
+
+   ff_driver.get(summaryPage)
+   ff_driver.switch_to.frame('ListDoc')
+   list_of_links = ff_driver.find_elements(by=By.XPATH,value='//a')
+   to_scrape=[]
+   for link in list_of_links:
         to_scrape.append(link.get_attribute('href'))
 
-    for click in to_scrape:
+   for click in to_scrape:
         print(click)
+        #TODO : don't download if file exists
         try:
             ff_driver.get(click)
+            sleep(2)
         except TimeoutException:
             pass
-    ff_driver.quit()
+
+   files = ff_driver.get_downloadable_files()
+   not_finished=True 
+   while (not_finished == True):
+       not_finished = testFinished(files)
+       files = ff_driver.get_downloadable_files()
+ 
+   for file in files:
+       ff_driver.download_file(file, "/home/dockerapp/europress/pages")
+ 
+   print("ScrappingPdf -- Finished")
+
+def find_edition(name,date_arg,edition,ff_driver):
+  print("Find Edition -- Start")
+  edition_string=''
+  if '' != date_arg : 
+   edition_string=date_arg
+  else :
+   url_pub='https://nouveau-europresse-com.bnf.idm.oclc.org/Pdf'
+   ff_driver.get(url_pub)
+   sleep(2)
+   try: 
+      sel_search=ff_driver.find_element(By.ID,'sources-search')
+      ActionChains(ff_driver)\
+         .send_keys_to_element(sel_search,name)\
+         .perform()
+      sleep(6)
+      css_selectorString='[id="sourceLastEdition"][pubid="'+edition+'_P"]'
+      sel_docs=ff_driver.find_element(By.ID, 'docList')
+      sel_edition=sel_docs.find_element(By.CSS_SELECTOR,css_selectorString) 
+      edition_string=sel_edition.get_attribute("lastedition").replace("-","")
+   except selenium.common.exceptions.NoSuchElementException:
+      print("Find Edition -- No element")
+ 
+   return (edition_string) 
+   print("Find Edition -- End")
 
 def main(argv):
+   print("Main -- Start")
+   print("Main -- Args reading")
+   
    journalname = ''
    editiondate = ''
+   scrapPDF=True
+
    try:
-      opts, args = getopt.getopt(argv,"hj:e:",["journal=","edition="])
+      opts, args = getopt.getopt(argv,"shj:e:",["journal=","edition="])
    except getopt.GetoptError:
       print ('EuropesseScrapping.py -j <journalname> -e <editiondate>')
       sys.exit(2)
@@ -76,68 +190,96 @@ def main(argv):
          journalname = arg
       elif opt in ("-e", "--edition"):
          editiondate = arg
-   
+      elif opt in ("-s" ):
+         scrapPDF=False
 
+   print("Main -- Read Config")
    conf= configparser.ConfigParser(interpolation=None)
-   conf.read('.\config.ini')
-   download_dir=conf['env']['download_dir']
-   result_dir=conf['env']['result_dir']
-   profile_path =conf['env']['profile_path']
-   service =conf['env']['service']
+   conf.read(sys.path[0]+'/config.ini')
+
    auth_url=conf['bnf']['auth_url']
    bnf_user=conf['bnf']['username']
    bnf_pwd=conf['bnf']['password']
+   download_dir=conf['env']['download_dir']
+   result_dir=conf['env']['result_dir']
 
-   
    press_code={} 
    for option in conf.options('europresse'):
         press_code[option] = conf.get('europresse', option)
-   
-   journalID=''
-   if journalname == '':
-       journalID='LM'
+   print(press_code)
+
+
+   # TODO : effacement avant dernier fichier telechargé. 
+   if journalname=='' :    
+      journalID="LM"
+      journalname="monde"
    elif journalname in press_code:
        journalID=press_code.get(journalname)
+       print("ID:"+journalID)
+       print("Name:"+journalname)
    else :
        print('journal should be one of the following')
        print(press_code.keys())
        sys.exit(2)
 
-   if editiondate=='':
-        journal_date=date.today()
-        if journalID=='LM':
-            journal_date=journal_date+timedelta(days=1)
-            
-        date_string=journal_date.strftime('%Y%m%d')
-   else :
-        date_string=editiondate
-
-   print("Recupere edition du " + date_string)
-   ScrappingTest(journalID,date_string,profile_path,service,auth_url,bnf_user,bnf_pwd)
-
-   print("Merge PDF")
-   sleep(5)
-
-   glob_pattern    ="\pdf?"+date_string+"?"+journalID+"_p*[!)].pdf"
-   glob_pattern_del="\pdf?"+date_string+"?"+journalID+"_*.pdf"
+   download_dir=conf['env']['download_dir']
+   result_dir=conf['env']['result_dir']
+   Tel_api_key=conf['telegram']['api']
+   Tel_channel_id=conf['telegram']['channel']
 
 
-   pdfs_to_merge = filter( os.path.isfile,
-                        glob.glob(download_dir + glob_pattern) )
-    
-    # Sort list of files based on last modification time in ascending order
-   pdfs_to_merge = sorted( pdfs_to_merge,
-                        key = os.path.getmtime)
-   merge_pdfs(pdfs_to_merge,output=result_dir + "\pdf_"+date_string+"_"+journalID+".pdf")
+   print("Main -- Scrap") 
 
-   print("Merge PDF OK - Nettoyage")
-   pdfs_to_del = filter( os.path.isfile,
-                        glob.glob(download_dir + glob_pattern_del) )
-   for pdf in pdfs_to_merge:
-      print(pdf)
-      os.remove(pdf)
-   print("Finished")
+   selenium_driver=BNF_auth( auth_url,bnf_user,bnf_pwd)
 
+   last_date_downloaded = find_last_pdf(result_dir, journalID)
+  
 
+   print("Main -- Last Download:" +last_date_downloaded) 
+   date_string=find_edition(journalname,editiondate,journalID,selenium_driver)
+   
+   print("Main -- Edition :"+date_string) 
+   if (date_string != '') and ((last_date_downloaded != date_string) or (last_date_downloaded == '' )) : 
+      
+       output_file=result_dir + "/pdf_"+date_string+"_"+journalID+".pdf"
+
+       if scrapPDF:
+          ScrappingPdf(date_string,journalID,selenium_driver)
+          print("Main -- Merge")
+
+          glob_pattern    ="/pdf?"+date_string+"?"+journalID+"_p*[!)].pdf"
+          glob_pattern_del="/pdf?"+date_string+"?"+journalID+"_*.pdf"
+          glob_pattern_rename="/pdf?"+date_string+"?"+journalID+"_p?[!)].pdf"
+
+          pdfs_to_rename = filter( os.path.isfile,
+                            glob.glob(download_dir + glob_pattern_rename) )
+
+          rename_pdfs(pdfs_to_rename)
+          print("Main -- Rename OK")
+          
+          pdfs_to_merge = filter( os.path.isfile,
+                            glob.glob(download_dir + glob_pattern) )
+         
+          pdfs_to_merge = sorted( pdfs_to_merge )
+          print("Main -- Merge OK")
+          
+          merge_pdfs(pdfs_to_merge,output=output_file)
+
+          pdfs_to_del = filter( os.path.isfile,
+                            glob.glob(download_dir + glob_pattern_del) )
+
+          for pdf in pdfs_to_del:
+             print(pdf)
+             os.remove(pdf)
+
+          print("Main -- Send")
+          asyncio.run(doc_tel(Tel_api_key,Tel_channel_id,output_file))
+   elif (date_string == '') : 
+          asyncio.run(msg_tel(Tel_api_key,Tel_channel_id,"Pb Recuperation Edition"))
+
+   selenium_driver.quit()
+   print("Main -- Finished")
+   
 if __name__ == "__main__":
    main(sys.argv[1:])
+
--- README.md
+++ README.md
@@ -4,17 +4,13 @@ This is a  selenium test script to generate a PDF file from a newspaper specific
 Remember that this PDF falls under international copyrights laws.
 I am not responsible for the use you make of this script or of its result.
 
-This is meant to be a proof of concept and was a project for me to start coding in python and use selenium.
+This is meant to be a proof of concept and was a hobby to start coding in python and use selenium.
 
 ## Prerequisites 
 
 - You'll need a reader card from the BNF (Bibliotheque Nationale de France) for this to work  
-- This script uses the selenium geckodriver  
-- You'll need to create a specific profile in Firefox (or any gecko based browser) 
-	- Download directory should be set to an empty directory
-	- Preference for handling application/pdf mime type files should be set to download
-	- Uncheck the box to confirm download location
-- PyPDF2 and selenium python library (see requirements.txt)
+- This script uses a selenium docker image (chromium)
+- PyPDF2, selenium and telegram bot python library (see requirements.txt)
 
 ## Configuration
 
@@ -25,10 +21,8 @@ This is meant to be a proof of concept and was a project for me to start coding 
 | ----------- | ----------- | ----------- |
 | bnf |  username  | user account at BNF.FR       |
 | bnf | password   | user account password at BNF.FR        |
-| env | profile_path   | the path where your Firefox profile is located        |
-| env | download_dir   | the download path specified in your Firefox profile   |
-| env | service   | location of the geckodriver   |
-| env | result_dir   | directory where the pdf will be generated   |
+| env | download_dir   | the download path for individual pages download   |
+| env | result_dir   | directory where the merged pdf will be generated  |
 
 ## Args
 
@@ -37,11 +31,12 @@ This is meant to be a proof of concept and was a project for me to start coding 
 
 ## Newspaper value
 
-The newspaper names are listed in the config.ini file.
+The newspaper names are listed in the config.ini file. 
 
-Note that the correct date of publication for evening newspapers is handled by the script 
+They are chosen to enable searching for the last edition available
+
+Some newspapers are available on europesse only 24h after public release 
 
 ## Todo 
 
-- Exception handling
-- Configure wait time
+- A lot
